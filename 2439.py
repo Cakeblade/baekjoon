946f1240526644c2cfc3986d0c3d9b21e4e5b11a
Cakeblade <jch1070@gmail.com>
2020-03-06 18:40:53 +0900

    Create 2439.py

---
+++ 2439.py
@@ -0,0 +1,12 @@
+import sys
+
+inputl = sys.stdin.readline
+
+n = int(inputl())
+
+for i in range(1, n + 1):
+	for j in range(n - i):
+		print(' ', end='')
+	for j in range(i):
+		print('*', end='')
+	print()
